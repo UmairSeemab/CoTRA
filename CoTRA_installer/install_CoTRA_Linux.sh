2adee9daf293376babfb61ee9976fb3ee3c62e9b
Umair <46960157+UmairSeemab@users.noreply.github.com>
2026-06-04 09:57:54 +0300

    Delete CoTRA_installer directory

--- CoTRA_installer/install_CoTRA_Linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting CoTRA package installation..."
-
-Rscript install_cotra_packages.R
-
-echo "Installation finished."
-read -p "Press Enter to close..."
--- CoTRA_installer/install_CoTRA_window.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting CoTRA package installation...
-
-Rscript install_cotra_packages.R
-
-echo.
-echo Installation finished.
-pause
